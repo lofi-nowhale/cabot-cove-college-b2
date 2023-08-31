2e5114f23fb4526791b8b1c79182daeeacdf6779
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-08-31 16:10:43 -0600

    debug: model files for validations and relationship set up

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,7 @@
 class Course < ApplicationRecord
+  # belongs_to :resident_courses
+  has_many :resident_courses
+  has_many :residents, through: :resident_courses
 
+  validates_presence_of :name
 end
--- app/models/resident.rb
+++ app/models/resident.rb
@@ -1,3 +1,8 @@
 class Resident < ApplicationRecord
+  has_many :resident_courses
+  has_many :courses, through: :resident_courses
 
+  validates_presence_of :name
+  validates_presence_of :age
+  validates_presence_of :occupation
 end
